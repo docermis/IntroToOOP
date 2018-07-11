206600348e877ccaca76e3a2ed3cafbe917d30e8
eioannidis <eioannidis@cite.gr>
2018-07-11 17:26:57 +0300

    testing

---
+++ IntroToOOP/EntryPoint.cs
@@ -0,0 +1,21 @@
+﻿using PoinstAndLines;
+using System;
+
+class EntryPoint
+{
+	static void Main()
+	{
+		Point point = new Point(5, 3);
+
+		Point pointTwo = new Point();
+
+		pointTwo.X = 5;	
+
+		Console.WriteLine(point.X);
+		Console.WriteLine(point.Y);
+
+		Console.WriteLine("Press enter to close...");
+		Console.ReadLine();
+	}
+}
+
---
+++ IntroToOOP/PointsAndLines/Point.cs
@@ -0,0 +1,32 @@
+﻿namespace PoinstAndLines
+{
+	class Point
+	{
+		private int x;
+		private int y;
+
+		public int X { get; set; }
+		public int Y
+		{
+			get
+			{
+				return y;
+			}
+			set
+			{
+				y = value;
+			}
+		}
+
+		public Point()
+		{
+
+		}
+
+		public Point(int x, int y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+	}
+}
